579cd4aa0891be44f961b743a681715adf8fca17
psa <spamiria@gmail.com>
2018-09-20 15:53:36 +0200

    instalacion de AWX

---
+++ 15-Tower/Git/install.sh
@@ -0,0 +1,7 @@
+apt-get install docker.io build-essential -y
+apt-get install python-pip -y
+pip install docker.py
+apt-get install npm -y
+apt-get install -f 
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
